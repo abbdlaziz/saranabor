792b67fb0fb9e9df0723c00a5e26ca9e90f1fdda
abbdlaziz <43935016+abbdlaziz@users.noreply.github.com>
2019-01-17 11:54:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>BillSmith</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+
+<header id="header">
+  <div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="intro-text">
+          <h1>BILLSMITH</h1>
+          <hr>
+          <p><font color="#FFFFFF"> • Mining Tools • Spare Part • </font></p>
+          <a href="#about" class="btn btn-default btn-lg page-scroll"><font color="#FFFFFF"> MORE FROM US </font></a> </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div id="nav">
+  <nav class="navbar navbar-custom">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> <i class="fa fa-bars"></i> </button>
+      </div>
+      
+      
+      <div class="collapse navbar-collapse navbar-main-collapse">
+        <ul class="nav navbar-nav">
+          
+          <li class="hidden"> <a href="#page-top"></a> </li>
+          <li> <a class="page-scroll" href="#about"><b><font size="+1">Profiles</font></b></a> </li>
+          <li> <a class="page-scroll" href="#portfolio"><b><font size="+1">Products</font></b></a> </li>
+          <li> <a class="page-scroll" href="#contact"><b><font size="+1">Find Us</font></b></a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Company Overview</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center"><img src="img/overview.jpg" class="img-responsive"></div>
+      <div class="col-md-8 col-md-offset-2">
+        <div class="about-text">
+          <h5><p>PT. SARANABOR BUMI PERKASA</p></h5>
+          <marquee direction="up" style="background:url(img/about.jpg)" scrollamount="1"><font color="#F8F3F3"><p>PT. SARANABOR BUMI PERKASA berdiri sebagai kelanjutan dari CV SARANABOR & CHEMINDO berdiri sejak 18 Desember 2002.  Mulai 13 Desember 2011 dilakukan pengembangan usaha aksesories alat bor untuk eksplorasi bahan tambang; mineral dan batubara; seismic serta geoteck dengan nama produk  ‘Bill Smith’.</p>
+          <p>Pendiri perusahaan berdedikasi Teknik Pertambangan, sadar betul dengan kekayaan SDA di Indonesia berniat supaya dapat mensejahterakan rakyat Indonesia. Tujuan mendirikan perusahaan ini juga memberikan kemudahan penyediaan aksesories alat bor yang bermutu dan memenuhi standar DCDMA (Diamond Core Drilling Manufactures Association) untuk eksplorasi bahan tambang di wilayah Indonesia, dimana aksesories tersebut sebelumnya sangat susah diperoleh yang harus import dari negara luar.</p>
+          <p>Dengan keberadaan PT. Saranabor Bumi Perkasa, telah banyak perusahaan pengeboran dan pertambangan dibantu. Baik di wilayah Indonesia ataupun di luar negeri terutama di Malaysia. Peralatan mudah didapat dan segera secepatnya diterima di lapangan. Demikian juga harga kompetitive dibandingkan dengan barang impor. Mutu produk tetap kami utamakan  berkwalitas ekspor.</p>
+          <p>PT. Saranabor Bumi Perkasa murni perusahaan nasional. Tenaga kerja 100% bangsa Indonesia terlatih. Saat ini lokasi pabrik menempati lahan seluas 1200 m² yang berdekatan dengan gudang seluas 1300 m2 di daerah Jatiasih-Bekasi. Untuk mempertahankan kwalitas produk, pengontrolan  pekerja  terampil serta  fasilitas pendukung dengan unit produksi mesin-mesin CNC.</p>
+          <p>Produk kami sudah banyak dipakai perusahaan BUMN, Swasta Nasional serta ekspor. Dalam rangka memenuhi kebutuhan customer kami jug membantu menerima design/rancangan khusus untuk aksesories bor yang dikehendaki oleh pemakai (end user).</p>
+          <p>Optimis dengan tujuan baik PT. SARANABOR BUMI PERKASA akan banyak memberikan kebaikan bagi Indonesia serta semua orang khususnya yang bergelut di dunia pertambangan. </p></marquee></font>
+          <a href="#portfolio" class="btn btn-default btn-lg page-scroll">Company Products</a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Our Products</h2>
+      <hr>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 graphic">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/1.jpg" title="Digunakan untuk pengeboran umum. Matriks logam bubuk memastikan ketahanan abrasi yang kuat dan umur pemakaian yang lama. Desain geometrik canggih diadopsi untuk penetrasi cepat. Ukuran standar 56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Non Coring Core Drill Bits</h4>
+              </div>
+              <img src="img/portfolio/1.jpg" class="img-responsive" alt="Digunakan untuk pengeboran umum. Matriks logam bubuk memastikan ketahanan abrasi yang kuat dan umur pemakaian yang lama. Desain geometrik canggih diadopsi untuk penetrasi cepat.Ukuran standar 56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 illustration">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/2.jpg" title="Polycrystalline diamond bits telah terbukti berhasil dalam formasi konsolidasi lunak hingga menengah. Mereka banyak digunakan dalam eksplorasi batu bara dan untuk pengeboran lubang terbuka ke zona menarik sebelum mulai pengeboran inti. Drainase gas metana di tambang batubara dan pengeboran horizontal untuk teknologi parit kurang adalah aplikasi penting untuk produk ini.
+Ukuran standar 55, 65, 76, 96, 99 & 123mm Diameter" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>PCD Non Core Bits</h4>
+              </div>
+              <img src="img/portfolio/2.jpg" class="img-responsive" alt="Polycrystalline diamond bits telah terbukti berhasil dalam formasi konsolidasi lunak hingga menengah. Mereka banyak digunakan dalam eksplorasi batu bara dan untuk pengeboran lubang terbuka ke zona menarik sebelum mulai pengeboran inti. Drainase gas metana di tambang batubara dan pengeboran horizontal untuk teknologi parit kurang adalah aplikasi penting untuk produk ini.
+Ukuran standar 55, 65, 76, 96, 99 & 123mm Diameter"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 graphic">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/3.jpg" title="
+Helical Pad Reamer konsep 4 in 1 dengan menggabungkan berlian alami dan tungsten carbide set dalam campuran berlian matriks diresapi. Formula ini memenuhi kriteria sebagian besar aplikasi pengeboran dari yang lebih kasar hingga formasi berbutir halus yang ditemukan di seluruh dunia. Ukuran standar 58, 68, 76, 96, 99 & 115mm Diameter" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Reamer</h4>
+              </div>
+              <img src="img/portfolio/3.jpg" class="img-responsive" alt="Helical Pad Reamer konsep 4 in 1 dengan menggabungkan berlian alami dan tungsten carbide set dalam campuran berlian matriks diresapi. Formula ini memenuhi kriteria sebagian besar aplikasi pengeboran dari yang lebih kasar hingga formasi berbutir halus yang ditemukan di seluruh dunia. Ukuran standar 58, 68, 76, 96, 99 & 115mm Diameter"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 graphic">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/4.jpg" title="Biasanya dipasok dengan tabung dalam split baja; namun tabung dalam plastik (non-split) tersedia dalam ukuran NMLC dan stainless dan hard split split chrome dapat disediakan untuk sebagian besar ukuran lainnya. Ukuran standar 	56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4> NMLC Part.</h4>
+              </div>
+              <img src="img/portfolio/4.jpg" class="img-responsive" alt="Biasanya dipasok dengan tabung dalam split baja; namun tabung dalam plastik (non-split) tersedia dalam ukuran NMLC dan stainless dan hard split split chrome dapat disediakan untuk sebagian besar ukuran lainnya.Ukuran standar 	56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 illustration">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/5.jpg" title="Kami memproduksi dan menyimpan berbagai peralatan pengeboran: ini termasuk inti barel dari TT56 hingga 8C, (yang termasuk ukuran paling umum N, NMLC & H) itu juga mencakup semua penggantian dan suku cadang yang diperlukan untuk semua ukuran barel. Ukuran standar 	56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Core Barrel</h4>
+              </div>
+              <img src="img/portfolio/5.jpg" class="img-responsive" alt="Kami memproduksi dan menyimpan berbagai peralatan pengeboran: ini termasuk inti barel dari TT56 hingga 8C, (yang termasuk ukuran paling umum N, NMLC & H) itu juga mencakup semua penggantian dan suku cadang yang diperlukan untuk semua ukuran barel. Ukuran standar 	56mm, 59mm, 75mm, 89mm, 91mm, 108mm, 110mm, 127mm, 131mm, 150mm, 170mm, 219mm, 275mm"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/6.jpg" title="Impregnated Core Bits merupakan mata bor mineral dan batu bara dimana matriks yang dibawah kondisi tertentu dapat mengikis pada tingkat maksimum. Sebagai diamond aktif yang menjadi tumpul, mereka akan dirilis dan diamond baru yang tajam akan melanjutkan pemotongan. Ukuran standar 59, 76, 93, 112, 132, 152 mm " data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Impregnated core Bits</h4>
+              </div>
+              <img src="img/portfolio/6.jpg" class="img-responsive" alt="Impregnated Core Bits merupakan mata bor mineral dan batu bara dimana matriks yang dibawah kondisi tertentu dapat mengikis pada tingkat maksimum. Sebagai diamond aktif yang menjadi tumpul, mereka akan dirilis dan diamond baru yang tajam akan melanjutkan pemotongan. Ukuran standar 59, 76, 93, 112, 132, 152 mm"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/7.jpg" title="
+Di Saranabor kami memproduksi dan menyimpan berbagai suku cadang dan peralatan untuk melayani industri pengeboran" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Core Barrel Spares</h4>
+              </div>
+              <img src="img/portfolio/7.jpg" class="img-responsive" alt="Di Saranabor kami memproduksi dan menyimpan berbagai suku cadang dan peralatan untuk melayani industri pengeboran"> </a> </div>
+          </div>
+        </div>
+       </div>
+    </div>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Location</h2>
+      <hr>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+         
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.677773683919!2d106.95711871412702!3d-6.305998163467628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69928883d07a4b%3A0xf9952ffb8930fe07!2sJl.+Raya+Villa+Jatirasa%2C+Jatiasih%2C+Kota+Bks%2C+Jawa+Barat!5e0!3m2!1sen!2sid!4v1525327315238" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+      </form>
+      <li>MAIN OFFICE : Jl. Villa Jatirasa No. 10 Rt. 01 Rw. 016
+Kp. Kebantenan Jatiasih – Bekasi 17423
+Indonesia
+</li>
+<li>PHONE : (021) 82433019</li>
+<li>FAX : (021) 82404927</li>
+<li>MAIL : saranabor@gmail.com
+</li>
+      <div class="social">
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>       
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; 2018 BillSmith.</p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
